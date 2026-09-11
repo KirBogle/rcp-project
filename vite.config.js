0ab568418f72c2f66e1339a6baf5334fdb897f2a
Белоусов Егор Сергеевич <24504@dune.edu>
2026-09-11 09:31:39 +0300

    add form

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+      oxc: {
+    jsx: {
+      include: [/src\/.*\.(js|jsx)$/],
+    },
+  },
 })
